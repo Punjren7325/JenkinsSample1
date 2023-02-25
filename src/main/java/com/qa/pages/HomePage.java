aabba295fcf427e77221b95161ec611847117b33
Punjren7325 <Punjren7325@gmail.com>
2023-02-25 10:41:21 -0500

    adding new files for Jenkins trial project

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+public class HomePage {
+//NOTES:: push new folder structure to git 
+//init git: type these at the command prompt::: 
+	//git init
+	//git remote add origin https://github.com/Punjren7325/NAME_OFnewfolder.git
+	//
+	public static void main (String args[])
+	{
+		System.out.println("HEllo world!");
+	}
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestUtil {
+
+	public static void main(String args[])
+	{
+		System.out.println("HEllow in testutil");
+	}
+}
